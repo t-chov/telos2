2add56f134c0512f0bead7ccc9d6e9ba87a451f0
Kohei Tsuyuki <kotsuyuki@gmail.com>
2018-01-07 12:40:15 +0000

    [add] show tasks

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -11,8 +11,14 @@
           <th>Actions</th>
         </thead>
         <tbody>
-          <tr v-for="task in tasks">
+          <tr v-for="(task, key) in tasks">
             <td>{{ task.name }}</td>
+            <td>{{ task.interval }}</td>
+            <td>{{ task.last_date }}</td>
+            <td>
+              <a class="button is-primary" v-on:click="doTask(key)">Done</a>
+              <a class="button is-danger" v-on:click="deleteTask(key)">Delete</a>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -21,15 +27,34 @@
 </template>
 
 <script>
-// import firebase from 'firebase';
-// import Vuex from 'vuex';
-// import { firebaseAction } from 'vuexfire';
+import firebase from 'firebase';
 import Navbar from './Navbar';
 
 export default {
   name: 'Tasks',
   components: {
     'my-navbar': Navbar,
+  },
+  methods: {
+    doTask(key) {
+      alert(key);
+    },
+    deleteTask(key) {
+      alert(key);
+    },
+  },
+  data() {
+    return {
+      user: firebase.auth().currentUser,
+      tasks: {},
+    };
+  },
+  created() {
+    const currentUser = firebase.auth().currentUser;
+    // eslint-disable-next-line
+    firebase.database().ref(`tasks/${currentUser.uid}`).once('value').then((snapshot) => {
+      this.tasks = snapshot.val();
+    });
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-// import Vuex from 'vuex';
 import firebase from 'firebase';
 import firebaseConfig from '../config/firebase';
 import App from './App';
@@ -10,28 +9,15 @@ import router from './router';
 require('bulma/css/bulma.css');
 require('font-awesome/css/font-awesome.css');
 
-// initialize in config/firebase.js
 firebase.initializeApp(firebaseConfig);
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 
 Vue.config.productionTip = false;
-// Vue.use(Vuex);
-
-// const store = new Vuex.Store({
-  // state: {
-    // tasks: [],
-    // user: null,
-  // },
-  // mutations: {
-    // ...firebaseMutations
-  // }
-// });
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  // store,
   template: '<App/>',
   components: { App },
 });
